7436686070500c6a056b65c071d7691d2a4d702d
Sondos Amr <sondos.amr444@gmail.com>
2025-01-12 16:45:59 +0200

    Create basic structure of the app with components : Logo , Form , PackingList , States

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">test</header>
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
     </div>
   );
 }
 
-export default App;
+function Logo() {
+  return <h1> 🌴 FAR AWAY 🧳</h1>;
+}
+
+function Form() {
+  return (
+    <form className="add-form">
+      <h3>What do you need for your 😎 trip ?</h3>
+      <input type="text" placeholder="Item..." />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function PackingList() {
+  return <div className="list"></div>;
+}
+
+function Stats() {
+  return (
+    <footer className="stats">
+      <em> You have x item on your list, and you already packed x (x%)</em>
+    </footer>
+  );
+}
